a294c11c656becb516843866be056d4274561201
AAngel777 <105814810+AAngel777@users.noreply.github.com>
2022-06-03 10:34:13 -0700

    ArchivosObjeto

---
+++ ArchivosObjeto/ArchivosObjeto/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+namespace ArchivosObjeto
+{
+    class Persona
+    {
+        public string nombre;
+        public long num;
+        public float prom;
+       public Persona(string nombre, long num, float prom)
+        {
+            this.nombre = nombre;
+            this.num = num;
+            this.prom = prom;
+        }
+        public void Escribe()
+        {
+            StreamWriter sw = new StreamWriter("Alumnos.txt", true);
+            sw.WriteLine(nombre + num + prom);
+            sw.Close();
+
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Ingrese Nombre: ");
+                string nombre = Console.ReadLine();
+                Console.Write("Ingrese numero de control: ");
+                long num = long.Parse(Console.ReadLine());
+                Console.Write("Ingrese promedio: ");
+                float prom = float.Parse(Console.ReadLine());
+                Persona o1 = new Persona(nombre,num,prom);
+                o1.Escribe();
+                Console.WriteLine("Escribiendo...");
+                Console.ReadLine();
+            }
+           catch(IOException e)
+            {
+                Console.WriteLine(e);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+
+            }
+ 
+        }
+    }
+}
